7b5c008f9b9165335c95f1577975ca399db1a7d6
Marshal John <marshaljohn2006@gmail.com>
2026-05-26 17:03:56 +0100

    do

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import Navbar from './Components/Navbar'
+import Hero from './Components/Hero'
+import Section1 from './Components/Section1'
+import Section2 from './Components/Section2'
+import Section3 from './Components/Section3'
+import Footer from './Components/Footer'
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <Hero/>
+      <Section1/>
+      <Section2/>
+      <Section3/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+// import heroImg from '../assets/Hero-img.png'
+
+function Hero() {
+  return (
+    <>
+        <div className='heroSection w-screen h-screen'>
+            {/* <img src= {heroImg} alt='Hero-img' className='heroImg w-screen h-[100vh]'/>       */}
+        </div>
+    
+    </>
+  )
+}
+
+export default Hero
